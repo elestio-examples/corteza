8a7449c359b4991a5555a95480b2ba7e308cc28c
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-09 09:42:05 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,10 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 30s;
+
+docker-compose exec -T server corteza-server settings set auth.mail.from-address ${SMTP_FROM}
+docker-compose down;
+docker-compose up;
+
+sleep 30s;
+docker-compose exec -T server corteza-server users add ${ADMIN_EMAIL} --password ${ADMIN_PASSWORD}
